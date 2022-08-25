7f841fbb40df7857ed33d416edce0aba46394599
Yevhenii Yakovenko <yakovenkob@gmail.com>
2022-08-25 21:13:08 +0300

    homework7

---
+++ js/01-gallery.js
@@ -0,0 +1,58 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery({ preview, original, description }) {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img 
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a >
+    </div>`
+}
+
+function addGallery(value) {
+    const gallery = value.map(createGallery).join('');
+    galleryContainer.innerHTML = gallery;
+}
+
+addGallery(galleryItems);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    createLightbox()
+}
+
+function createLightbox() {
+    const instance = basicLightbox.create(
+        `
+    <img src="${event.target.dataset.source}">`,
+        {
+            onShow(instance) {
+                window.addEventListener("keydown", RemoveEventListener);
+            },
+            onClose(instance) {
+                window.removeEventListener("keydown", RemoveEventListener);
+            },
+        }
+    );
+    instance.show();
+
+    function RemoveEventListener(event) {
+        if (event.code !== "Escape") {
+            return;
+        }
+        instance.close();
+    }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery({ preview, original, description }) {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+function addGallery(value) {
+    const gallery = value.map(createGallery).join('');
+    galleryContainer.innerHTML = gallery;
+
+    makeNewGallery();
+}
+
+function makeNewGallery() {
+    new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionsDelay: 250,
+    });
+}
+
+addGallery(galleryItems);
